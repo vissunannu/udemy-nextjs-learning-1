d18891f61a55ef735f0cb24591c15441b618e640
G Ramesh <ramesh.guthula.kkd@gmail.com>
2021-03-16 20:40:06 +0530

    first commit

---
+++ components/events/event-list.js
@@ -0,0 +1,25 @@
+import EvnetItem from './event-item';
+import classes from './event-list.module.css';
+
+
+function EventList(props) {
+
+    const { items } = props;
+
+    return (
+
+        <ul className={classes.list}>
+            {items.map(event => 
+            <EvnetItem
+                key={event.id}
+                id={event.id}
+                title={event.title}
+                location={event.location}
+                date={event.date}
+                image={event.image} 
+            />)}
+        </ul>
+    );
+}
+
+export default EventList;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,10 @@
+function EventDetail(){
+
+    return(
+      <div>
+          <h1>Event Detail page</h1>
+      </div>
+    );
+}
+
+export default EventDetail;
